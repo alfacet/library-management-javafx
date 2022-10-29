d225a5148c9a52214fcee44ae43a2e1468d3333e
nathanfl1 <nathanfilipe662@gmail.com>
2022-10-29 17:18:39 -0300

    implementation of searchBooks method

--- src/main/java/com/alface/AddBooksController.java
+++ src/main/java/com/alface/AddBooksController.java
@@ -19,51 +19,59 @@ import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class AddBooksController extends BigController {
 
     Dotenv dotenv = Dotenv.configure()
-        .directory("./.env")
-        .ignoreIfMalformed() //
-        .ignoreIfMissing()
-        .load();
+            .directory("./.env")
+            .ignoreIfMalformed() //
+            .ignoreIfMissing()
+            .load();
 
-    public void initialize() {
+    @FXML
+    Button searchButton;
+    @FXML
+    ListView<Label> booksList;
+    @FXML
+    TextField inputBookName;
+    @FXML
+    Label errorLabel;
+
+    public void searchBooks() {
         JsonReader reader;
-        Gson gson = new Gson();
-        String line;
-        StringBuilder responseContent = new StringBuilder();
         try {
-            URL link = new URL(dotenv.get("GOOGLE_API"));
-            HttpURLConnection conexao = (HttpURLConnection) link.openConnection();
+            if (inputBookName.getText().equals(""))
+                errorLabel.setText("This field cannot be empty!");
+            else {
+                String pesquisa = inputBookName.getText();
+                pesquisa = pesquisa.replaceAll(" ", "%20");
+                URL link = new URL(
+                        "https://www.googleapis.com/books/v1/volumes?q=" + pesquisa + ":keyes" + dotenv.get("GOOGLE_API"));
+                HttpURLConnection conexao = (HttpURLConnection) link.openConnection();
+                conexao.setRequestMethod("GET");
+                reader = new JsonReader(new InputStreamReader(conexao.getInputStream()));
+                JsonElement dividido = JsonParser.parseReader(reader);
+                JsonObject obj = dividido.getAsJsonObject();
+                JsonArray vetor = obj.get("items").getAsJsonArray();
 
-            conexao.setRequestMethod("GET");
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println("deu bom.");
-            reader = new JsonReader(new InputStreamReader(conexao.getInputStream()));
-            JsonElement dividido = JsonParser.parseReader(reader);
-            JsonObject obj = dividido.getAsJsonObject();
-            JsonArray vetor = obj.get("items").getAsJsonArray();
-            ArrayList<Book> lista = new ArrayList<Book>();
-            for (int i = 0; i < vetor.size(); i++) {
-                JsonObject temp = vetor.get(i).getAsJsonObject();
-                JsonObject info = temp.get("volumeInfo").getAsJsonObject();
-                 Book livro = new Book(info.get("title").toString(), 23);
-
-                System.out.println(livro.title);
+                ObservableList<Label> lista2 = FXCollections.observableArrayList();
+                for (int i = 0; i < vetor.size(); i++) {
+                    JsonObject temp = vetor.get(i).getAsJsonObject();
+                    JsonObject info = temp.get("volumeInfo").getAsJsonObject();
+                    Book livro = new Book(info.get("title").toString(), 23);
+                    lista2.add(new Label(livro.getTitle()));
+                }
+                booksList.setItems(lista2);
             }
-            
-        
         } catch (Exception e) {
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
+
             System.out.println(e);
         }
     }
