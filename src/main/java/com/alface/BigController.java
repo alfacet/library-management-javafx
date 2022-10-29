90434513f56d2ee6ded03179752d0ce834ed1b11
alfacet <filipedavi80@gmail.com>
2022-10-28 21:14:59 -0300

    change path of images in linux

--- src/main/java/com/alface/BigController.java
+++ src/main/java/com/alface/BigController.java
@@ -3,10 +3,27 @@ package com.alface;
 public class BigController {
     // Quando for colocar imagens, usa o path + o nome da imagem pra ficar mais
     // fácil c:
-    protected String pathCss = "file:\\\\\\" + System.getProperty("user.dir") + "\\src\\main\\java\\css";
-    protected String pathImages = "file:\\\\\\" + System.getProperty("user.dir") + "\\src\\main\\images\\";
+    protected String pathCss;
+    protected String pathImages;
     
     public BigController() {
-       
+      if(System.getProperty("os.name") == "Linux")
+       {
+        pathImages = System.getProperty("user.dir") + "/src/main/images/";
+        pathCss = System.getProperty("user.dir") + "/src/main/java/css/";
+       }
+       else
+       {
+        pathImages = "file:\\\\\\" + System.getProperty("user.dir") + "\\src\\main\\images\\";
+        pathCss = System.getProperty("user.dir") + "\\src\\main\\java\\css\\";
+       }
+    }
+    public String getPathImages()
+    {
+        return pathImages;
+    }
+    public String getPathCss()
+    {
+        return pathCss;
     }
 }
